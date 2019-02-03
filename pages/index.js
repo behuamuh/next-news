b815aa2c80315f3a2b822163f0f57658ada5aabd
Behuamuh <sven1980@list.ru>
2019-02-03 21:44:26 +0300

    add storylist

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,28 @@
+import fetch from "isomorphic-fetch";
+import Error from "next/error";
+import StoryList from "../components/StoryList";
+const URL = "https://node-hnapi.herokuapp.com/news?page=1";
+
 export default class Index extends React.Component {
+  static async getInitialProps() {
+    let stories;
+    try {
+      const responce = await fetch(URL);
+      stories = await responce.json();
+    } catch (err) {
+      console.error(err);
+      stories = [];
+    }
+    return { stories };
+  }
   render() {
-    return <div>Hacker Next</div>;
+    const { stories } = this.props;
+    if (!stories.length) return <Error statusCode="503" />;
+    return (
+      <div>
+        <h1>Hacker Next</h1>
+        <StoryList stories={stories} />
+      </div>
+    );
   }
 }
