a72815ce4954b9b8e5e71f6d84b704cd58695b22
Behuamuh <sven1980@list.ru>
2019-02-04 05:07:40 +0300

    add commentlist and commet

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,28 @@
 import fetch from "isomorphic-fetch";
 import Error from "next/error";
+import Link from "next/link";
 import Layout from "../components/Layout";
 import StoryList from "../components/StoryList";
-const URL = "https://node-hnapi.herokuapp.com/news?page=1";
 
 export default class Index extends React.Component {
-  static async getInitialProps() {
+  static async getInitialProps({ req, res, query }) {
     let stories;
+    let page;
+
     try {
-      const responce = await fetch(URL);
+      page = Number(query.page) || 1;
+      const responce = await fetch(
+        `https://node-hnapi.herokuapp.com/news?page=${page}`
+      );
       stories = await responce.json();
     } catch (err) {
       console.error(err);
       stories = [];
     }
-    return { stories };
+    return { stories, page };
   }
   render() {
-    const { stories } = this.props;
+    const { stories, page } = this.props;
     if (!stories.length) return <Error statusCode="503" />;
     return (
       <Layout
@@ -25,6 +30,24 @@ export default class Index extends React.Component {
         description="A hacker news clone with Next.js"
       >
         <StoryList stories={stories} />
+        <footer>
+          <Link href={`/?page=${page + 1}`}>
+            <a>Next page ({page + 1})</a>
+          </Link>
+        </footer>
+        <style jsx>{`
+          footer {
+            padding: 1em;
+          }
+          footer a {
+            font-weight: bold;
+            color: black;
+            text-decoration: none;
+          }
+          footer a:hover {
+            text-decoration: underline;
+          }
+        `}</style>
       </Layout>
     );
   }
